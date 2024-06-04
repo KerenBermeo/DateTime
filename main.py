2cf8b49d33b33fdcb4e67f7925556227adc56049
KerenBermeo <bermeokeren@gmail.com>
2024-06-04 15:42:30 -0500

    main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi import FastAPI
-from api import addsubtract, convert, current, dayofweek, difference, nextoccurrence, validate, weeknumber
+from api import addsubtract, convert, current, dayofweek, difference, weeknumber
 
 app = FastAPI()
 
@@ -8,7 +8,7 @@ def docs_route():
     return get_swagger_ui_html(openapi_url="/openapi.json", title="API Documentation")
 
 @app.get("/")
-def read_root():
+def main():
     return  docs_route()
 
 app.include_router(addsubtract.router)
@@ -16,6 +16,4 @@ app.include_router(convert.router)
 app.include_router(current.router)
 app.include_router(dayofweek.router)
 app.include_router(difference.router)
-app.include_router(nextoccurrence.router)
-app.include_router(validate.router)
 app.include_router(weeknumber.router)
